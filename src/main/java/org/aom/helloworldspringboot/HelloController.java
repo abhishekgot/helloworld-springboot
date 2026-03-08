880b00b26d2297833be87d33b1ded85909b87eb2
abhishekgot <abhishekomprakash4@gmail.com>
2026-03-08 19:54:41 +0100

    Added ConfigMap and Secret yaml and access the values

--- src/main/java/org/aom/helloworldspringboot/HelloController.java
+++ src/main/java/org/aom/helloworldspringboot/HelloController.java
@@ -1,11 +1,18 @@
 package org.aom.helloworldspringboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${MESSAGE:Hello Default}")
+    private String message;
+
+    @Value("${APP_SECRET:default-secret}")
+    private String appSecret;
 
     private final HelloService helloService;
 
@@ -15,6 +22,12 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(@RequestParam(required = false) String name) {
-        return helloService.getGreeting(name);
+        //return helloService.getGreeting(name);
+        return message;
+    }
+
+    @GetMapping("/secret")
+    public String secret() {
+        return appSecret;
     }
 }
